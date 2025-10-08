613540ab9b39801cf7891556f217a499092e1e22
infinite223 <dawidszmigiel9@gmail.com>
2025-10-08 19:17:48 +0200

    feat: added translations to stack page

--- app/[locale]/stack/page.tsx
+++ app/[locale]/stack/page.tsx
@@ -2,12 +2,23 @@
 
 import StackCard from "@/app/components/StackCard";
 import { fullStack } from "./consts";
+import { useTranslations } from "next-intl";
 
 export default function StackPage() {
+  const t = useTranslations("stackPage");
+
+  // Mapowanie nazw -> kluczy bez kropek
+  const keyMap: Record<string, string> = {
+    ".NET": "DotNET",
+    "Next.js": "Nextjs",
+  };
+
+  const getKey = (name: string) => keyMap[name] || name;
+
   return (
     <div className="flex flex-col gap-6 md:px-6">
       <div>
-        <h1 className="text-3xl font-semibold">Stack</h1>
+        <h1 className="text-3xl font-semibold">{t("title")}</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6">
           {fullStack.map((s, i) => (
@@ -15,7 +26,7 @@ export default function StackPage() {
               key={i}
               title={s.name}
               icon={s.icon}
-              description={s.description}
+              description={t(`descriptions.${getKey(s.name)}`)}
               color={s.color}
             />
           ))}
